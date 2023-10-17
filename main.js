b944f428588d04d3c6c663a96ddcd9cca8332c01
Konrad Dzwinel <kdzwinel@gmail.com>
2023-10-18 00:23:09 +0200

    Clean up fetching and showing data.

--- main.js
+++ main.js
@@ -1,19 +1,35 @@
 const fetch = require('node-fetch');
-const Address = require('./icao.js');
 const config = require('./CONFIG.js');
+const AIRPORTS = require('./airports.json');
+const AIRLINES = require('./airlines.json');
 
 const ALL_STATES_METHOD = 'states/all';
 const FLIGHT_INFO = 'flight';
 
 const OPENSKY_BASE_URL = 'https://opensky-network.org/api/';
 const AIRLABS_BASE_URL = 'https://airlabs.co/api/v9/';
+const AIRLABS_KEY = process.env.AIRLABS_KEY || config.airlabs_key;
+const LOCATION = {
+    latMin: process.env.LAT_MIN || config.location.latMin,
+    latMax: process.env.LAT_MAX || config.location.latMax,
+    lonMin: process.env.LON_MIN || config.location.lonMin,
+    lonMax: process.env.LON_MAX || config.location.lonMax
+};
+
+function findAirport(icao) {
+    return AIRPORTS.find(a => a.icao === icao);
+}
+
+function findAirline(icao) {
+    return AIRLINES.find(a => a.icao === icao);
+}
 
 async function callAPIMethod(method, params) {
     let baseURL = OPENSKY_BASE_URL;
 
     if (method === FLIGHT_INFO) {
         baseURL = AIRLABS_BASE_URL;
-        params['api_key'] = config.airlabs_key;
+        params['api_key'] = AIRLABS_KEY;
     }
 
     const url = new URL(baseURL);
@@ -28,37 +44,57 @@ async function getAllStates(location) {
     const data = await callAPIMethod(ALL_STATES_METHOD, { lamin: location.latMin, lamax: location.latMax, lomin: location.lonMin, lomax: location.lonMax });
     let states = [];
 
+    console.log(`Found ${data.states ? data.states.length : 0} states matching location.`);
+
     if (data.states) {
-        states = data.states.map(state => new State(state));
+        for (const stateInfo of data.states) {
+            stateInfo[1] = stateInfo[1].trim();
+            const callsign = stateInfo[1];
+            console.log(`Fetching flight data for ${callsign}.`);
+
+            const flightInfo = await getFlight(callsign);
+            states.push(new State(stateInfo, flightInfo));
+        }
     }
 
     return states;
 }
 
-async function getFlight(icao24) {
-    console.log(icao24, Address.ToTailNumber(`0x${icao24}`));
-    const data = await callAPIMethod(FLIGHT_INFO, { flight_icao: Address.ToTailNumber(`0x${icao24}`) });
+async function getFlight(callsign) {
+    const data = await callAPIMethod(FLIGHT_INFO, { flight_icao: callsign });
 
-    console.log(data);
+    return data && data.response;
 }
 
 class State {
-    constructor(inputArray) {
+    constructor(stateInfo, flightInfo) {
         const STATE_FIELDS = ['icao24', 'callsign', 'origin_country', 'time_position', 'last_contact', 'longitude', 'latitude', 'baro_altitude', 'on_ground', 'velocity', 'true_track', 'vertical_rate', 'sensors', 'geo_altitude', 'squawk', 'spi', 'position_source', 'category'];
+        STATE_FIELDS.forEach((fieldName, index) => this[fieldName] = stateInfo[index]);
 
-        STATE_FIELDS.forEach((fieldName, index) => this[fieldName] = inputArray[index]);
+        if (flightInfo) {
+            const FLIGHT_FIELDS = ['dep_icao', 'arr_icao', 'type', 'manufacturer', 'engine', 'model', 'built', 'airline_icao', 'airline_iata'];
+            FLIGHT_FIELDS.forEach(fieldName => this[fieldName] = flightInfo[fieldName]);
 
-        getFlight(this.icao24);
+            this.departure = findAirport(this.dep_icao);
+            this.arrival = findAirport(this.arr_icao);
+            this.airline = findAirline(this.airline_icao);
+        }
     }
 
     toString() {
-        console.log(`✈️ ${this.callsign} (${this.from}, ${this.to})`);
+        const airline = this.airline ? this.airline.name : '(unknown airline)';
+        const from = this.departure ? `${this.departure.city}/${this.departure.country}` : (this.dep_icao || '?');
+        const to = this.arrival ? `${this.arrival.city}/${this.arrival.country}` : (this.arr_icao || '?');
+
+        return `✈️ ${airline} ${this.callsign} (${from} → ${to})`;
     }
 }
 
 async function main() {
-    const states = await getAllStates(config.location);
-    states.forEach(state => console.log(state))
+    const states = await getAllStates(LOCATION);
+
+    console.log('Showing final data:');
+    states.forEach(state => console.log(state.toString()))
 }
 
 main();
