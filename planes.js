d914b114494f178b137055c0e7570d175fcec6ff
Konrad Dzwinel <kdzwinel@gmail.com>
2023-10-18 15:24:36 +0200

    fix a bug

--- planes.js
+++ planes.js
@@ -97,8 +97,8 @@ function createPlane(stateInfo, flightInfo) {
     const STATE_FIELDS = ['icao24', 'callsign', 'origin_country', 'time_position', 'last_contact', 'longitude', 'latitude', 'baro_altitude', 'on_ground', 'velocity', 'true_track', 'vertical_rate', 'sensors', 'geo_altitude', 'squawk', 'spi', 'position_source', 'category'];
     STATE_FIELDS.forEach((fieldName, index) => plane[fieldName] = stateInfo[index]);
 
-    if (this.callsign) {
-        this.airline_icao = guessAirlineIcaoFromCallsign(this.callsign);
+    if (plane.callsign) {
+        plane.airline_icao = guessAirlineIcaoFromCallsign(plane.callsign);
     }
 
     if (flightInfo) {
