e19c2dd7bb8e789ddbd13cdca1395868c458fa47
Konrad Dzwinel <kdzwinel@gmail.com>
2023-10-18 15:24:06 +0200

    Don't guess airline if no callsign

--- planes.js
+++ planes.js
@@ -97,7 +97,9 @@ function createPlane(stateInfo, flightInfo) {
     const STATE_FIELDS = ['icao24', 'callsign', 'origin_country', 'time_position', 'last_contact', 'longitude', 'latitude', 'baro_altitude', 'on_ground', 'velocity', 'true_track', 'vertical_rate', 'sensors', 'geo_altitude', 'squawk', 'spi', 'position_source', 'category'];
     STATE_FIELDS.forEach((fieldName, index) => plane[fieldName] = stateInfo[index]);
 
-    this.airline_icao = guessAirlineIcaoFromCallsign(this.callsign);
+    if (this.callsign) {
+        this.airline_icao = guessAirlineIcaoFromCallsign(this.callsign);
+    }
 
     if (flightInfo) {
         const FLIGHT_FIELDS = ['dep_icao', 'arr_icao', 'type', 'manufacturer', 'engine', 'model', 'built', 'airline_icao', 'airline_iata'];
